88c0661059d18bc6f6a94e2dd14141afe08b7726
Trent <125167749+TrentR0125@users.noreply.github.com>
2025-09-29 13:32:12 -0500

    typeorm init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './modules/client/client.module';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     CqrsModule.forRoot(),
+    ConfigModule.forRoot({ cache: true, isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: "mysql",
+        host: config.get<string>("MYSQL"),
+        port: config.get<number>("MYSQL_PORT"),
+        username: config.get<string>("MYSQL_USER"),
+        password: config.get<string>("MYSQL_PASS"),
+        database: config.get<string>("MYSQL_DATABASE"),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: config.get<string>("API_ENV") == "development"
+      }),
+    }),
     ClientModule
   ],
   controllers: [AppController],
