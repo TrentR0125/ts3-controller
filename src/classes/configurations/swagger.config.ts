f70ab0d8151be995005b26a3e2f0f71d6ed0df60
Trent <125167749+TrentR0125@users.noreply.github.com>
2025-09-29 13:38:53 -0500

    setup swagger

---
+++ src/classes/configurations/swagger.config.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export class SwaggerConfigSetup {
+    static async setup(app: INestApplication) {
+        const path = "swagger";
+        const config = new DocumentBuilder()
+            .setTitle("TS3 Controller API")
+            .setVersion("v1")
+            .build();
+
+        const document = SwaggerModule.createDocument(app, config);
+
+        SwaggerModule.setup(path, app, document, { customSiteTitle: "TS3 Controller" });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TsClient } from './classes/ts-client';
+import { TsClient } from './classes/teamspeak/ts-client';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  SwaggerModule.setup
   await TsClient.connect();
 
   await app.listen(process.env.PORT ?? 3000);
