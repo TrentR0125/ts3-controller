6b50ac55a416584a333a84caec5e3eee85ed4479
Trent <125167749+TrentR0125@users.noreply.github.com>
2025-09-29 13:43:11 -0500

    i realized i had a stroke

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TsClient } from './classes/teamspeak/ts-client';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerConfigSetup } from './classes/configurations/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
 
-  SwaggerModule.setup
+  SwaggerConfigSetup.setup(app);
   await TsClient.connect();
 
   await app.listen(process.env.PORT ?? 3000);
