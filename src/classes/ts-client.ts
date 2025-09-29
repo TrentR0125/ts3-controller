7e926587cdb21cf5c2ea5b2c6d105369954356ab
Trent <125167749+TrentR0125@users.noreply.github.com>
2025-09-29 13:10:00 -0500

    teamspeak connection

--- src/classes/ts-client.ts
+++ src/classes/ts-client.ts
@@ -2,7 +2,7 @@ import { TeamSpeak } from "ts3-nodejs-library";
 
 export let tsClient: TeamSpeak;
 export class TsClient {
-    async connect() {
+    static async connect() {
         tsClient = new TeamSpeak({
             host: "",
             queryport: 10001,
