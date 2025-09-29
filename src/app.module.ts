2404c957be8667584b1abd62980d45a887f9d202
Trent <125167749+TrentR0125@users.noreply.github.com>
2025-09-29 13:18:06 -0500

    add client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './modules/client/client.module';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [],
+  imports: [
+    CqrsModule.forRoot(),
+    ClientModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
